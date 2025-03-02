9ca6c408563d6cb2ed73f8772f5ca62f4cd4f372
martingom4 <martingomez9636@gmail.com>
2025-03-02 00:12:58 -0500

    listar transacciones en una lista

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const formulario = document.getElementById('formulario');
-    const resultado = document.getElementById('resultado');
+    const listaTransacciones = document.getElementById('lista-transacciones');
 
     formulario.addEventListener('submit', (evento) => {
         evento.preventDefault();
         const nombre = document.getElementById('nombre').value;
         const monto = document.getElementById('monto').value;
+        if (nombre === '' || monto === '') {
+            alert('Por favor, llena ambos campos');
+            return;
+        }
+        //crear un nuevo elemento de lista (li)
+        const nuevaEntrada = document.createElement('li');
+        nuevaEntrada.textContent = `📌 ${nombre}: $${parseFloat(monto).toFixed(2)}`;
+        //Agregar elemento a la lista
+        listaTransacciones.appendChild(nuevaEntrada);
+        formulario.reset();
 
-        //mostrar datos en pantalla
-        resultado.textContent = `Hola ${nombre}, el monto ingresado es: ${monto}`;
-        formulario.reset(); //limpiar formulario
     });
 });
