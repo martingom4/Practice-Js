44aee92847d122381483b83fb22653202e952486
martingom04 <martingomez9636@gmail.com>
2025-03-03 13:33:19 -0500

    React components

--- React/00-hola-mundo/src/main.jsx
+++ React/00-hola-mundo/src/main.jsx
@@ -5,11 +5,15 @@ import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.getElementById('root'))
 
+const OtroButton = ({text}) =>{
+  return <button>{text}</button>
+}
+
 root.render(
   <React.Fragment>
     <h1>Hello World</h1>
-    <button>Hello Button</button>
-    <button>Hello Button</button>
+    <OtroButton text="Click me"/>
+
   </React.Fragment>
 )
 
