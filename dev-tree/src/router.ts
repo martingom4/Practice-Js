ad4b1d40731446738b3711e307222f23785df2d8
martingom04 <martingomez9636@gmail.com>
2025-03-05 13:22:27 -0500

    Validacion de todos los parametros

--- dev-tree/src/router.ts
+++ dev-tree/src/router.ts
@@ -8,8 +8,18 @@ const router = Router()
 /* Autenticacion y registro */
 
 router.post('/auth/register',
-    body('handle').notEmpty(),
-
+    body('handle')
+        .notEmpty()
+        .withMessage('El handle de usuario es requerido'),
+    body('name')
+        .notEmpty()
+        .withMessage('El nombre de usuario es requerido'),
+    body('email')
+        .isEmail()
+        .withMessage('El email no es valido '),
+    body('password')
+        .isLength({min: 8})
+        .withMessage('La contraseña debe tener min 8 caracteres'),
     createAccount)
 
 export default router
