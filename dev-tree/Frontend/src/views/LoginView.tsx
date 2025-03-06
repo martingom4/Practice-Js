c04e0f14bc1dcc69eb624335e3e0ea221bb13eee
martingom04 <martingomez9636@gmail.com>
2025-03-05 19:21:16 -0500

    Comienzo de frontend con react

--- dev-tree/Frontend/src/views/LoginView.tsx
+++ dev-tree/Frontend/src/views/LoginView.tsx
@@ -1,11 +1,13 @@
-import {NavLink, Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 export default function LoginView(){
     return (
         <>
-        <div>
-            <h1>Login</h1>
-
+        <div className=' bg-slate-800 min-h-screen'>
+            <div className='max-w-lg mx-auto pt-10 px-5'>
+                <img src = '/logo.svg' alt='logo tipo de la app'/>
+                <div className=' text-white'>Login</div>
+            </div>
         </div>
         <nav>
             <Link to='/auth/register'>
