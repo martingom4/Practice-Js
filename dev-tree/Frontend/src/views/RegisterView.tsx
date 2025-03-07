5d392016bf97b959a94681cb9d0e7124b6f33e67
martingom04 <martingomez9636@gmail.com>
2025-03-07 17:21:25 -0500

    hooks con react con la libreari react-hook-form https://react-hook-form.com/get-started

--- dev-tree/Frontend/src/views/RegisterView.tsx
+++ dev-tree/Frontend/src/views/RegisterView.tsx
@@ -1,18 +1,106 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import {useForm} from 'react-hook-form'
 export default function RegisterView() {
+    const {register, handleSubmit, watch, formState: {errors}} = useForm();
+    console.log(errors)
+    const handleRegister = () => {
+        console.log('desde de handleRegister');
+    }
     return (
         <>
-            <div>
-                <h1 className=' text-white'>Register</h1>
+        <div>
+            <h1 className=" text-white text-4xl font-bold">Crear Cuenta</h1>
+        </div>
+        <form
+            onSubmit={handleSubmit(handleRegister)}
+            className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10">
 
+            <div className="grid grid-cols-1 space-y-3">
+            <label htmlFor="name" className="text-2xl text-slate-500">Nombre</label>
+                <input
+                    id="name"
+                    type="text"
+                    placeholder="Tu Nombre"
+                    className="bg-slate-200 border-none p-3 rounded-lg placeholder-slate-400"
+                    {...register('name', {
+                        required: 'El nombre es obligatorio'
+                    })}
+                />
             </div>
-            <nav>
-            <Link className= ' text-white' to='/auth/login'>
-                ¿Ya tienes cuenta? Inicia sesion aca
+            <div className="grid grid-cols-1 space-y-3">
+            <label htmlFor="email" className="text-2xl text-slate-500">
+                E-mail
+            </label>
+                <input
+                    id="email"
+                    type="email"
+                    placeholder="Email de Registro"
+                    className="bg-slate-200 border-none p-3 rounded-lg placeholder-slate-400"
+                    {...register('email', {
+                        required: 'El email es obligatorio'
+                    })}
+                />
+            </div>
+            <div className="grid grid-cols-1 space-y-3">
+            <label htmlFor="handle" className="text-2xl text-slate-500">Handle </label>
+            <input
+                id="handle"
+                type="text"
+                placeholder="Nombre de usuario: sin espacios"
+                className="bg-slate-200 border-none p-3 rounded-lg placeholder-slate-400"
+                {...register('handle', {
+                    required: 'El Handle es obligatorio'
+                })}
+            />
+            </div>
+            <div className="grid grid-cols-1 space-y-3">
+            <label htmlFor="password" className="text-2xl text-slate-500">
+                Password
+            </label>
+            <input
+                id="password"
+                type="password"
+                placeholder="Password de Registro"
+                className="bg-slate-200 border-none p-3 rounded-lg placeholder-slate-400"
+                {...register('password', {
+                    required: 'El password es obligatorio'
+                })}
+            />
+            </div>
+
+            <div className="grid grid-cols-1 space-y-3">
+            <label
+                htmlFor="password_confirmation"
+                className="text-2xl text-slate-500"
+            >
+                Repetir Password
+            </label>
+            <input
+                id="password_confirmation"
+                type="password"
+                placeholder="Repetir Password"
+                className="bg-slate-200 border-none p-3 rounded-lg placeholder-slate-400"
+                {...register('password_confirmation', {
+                    required: 'El password debe ser igual al anterior'
+                })}
+            />
+            </div>
+
+            <input
+            type="submit"
+            className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+            value="Crear Cuenta"
+            />
+        </form>
+
+        <nav className="mt-10 ">
+            <Link
+            className=" text-white text-center text-lg block"
+            to="/auth/login"
+            >
+            ¿Ya tienes cuenta? Inicia sesion aca
             </Link>
         </nav>
         </>
     );
-};
-
-
+}
