d972fcc97514016ae251965d62980622b2a4cfde
martingom04 <martingomez9636@gmail.com>
2025-03-12 21:15:16 -0500

    React prueba

---
+++ React/00-hola-mundo/index.js
@@ -0,0 +1,5 @@
+
+const numeros = [1, 2, 3,4];
+const [primero, segundo,tercero,cuarto] = numeros;
+console.log(primero, segundo,tercero,cuarto); // Imprime: 1 2
+// orueba con react para mantener el orde 
